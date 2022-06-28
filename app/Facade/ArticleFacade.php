0e35f86afcbcc1b235ee4c4958cf8ab1a9527037
zolotovjenya <zolotovjenya@gmail.com>
2022-06-28 13:13:32 +0300

    edited articles method

--- app/Facade/ArticleFacade.php
+++ app/Facade/ArticleFacade.php
@@ -7,28 +7,6 @@ use Illuminate\Support\Facades\Validator;
 use App\Classes\ArticleSingleton\ArticleExtendedSingleton;
 
 class ArticleFacade{
-    public function getAllArticles(){
-        $article = new Article();
-
-        /*
-        Firstly and lastly creation of object
-        */
-        $articleExtendedSingleton = ArticleExtendedSingleton::getInstance();
-
-        $articleExtendedSingleton->setAllArticles($article::all());
-
-        /*
-        We work with previous created object
-        */
-        $articleExtendedSingleton2 = ArticleExtendedSingleton::getInstance();
-
-        $articleExtendedSingleton->setAllArticles($article::all());
-
-        $data = $articleExtendedSingleton->getAllArticles();
-
-        return $data[0];
-    }
-
     public function getArticle($request){
         $validator = Validator::make($request->all(), [
             'id' => 'required',
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,15 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Article;
 use App\Facade\ArticleFacade;
+use App\Classes\ArticleSingleton\ArticleExtendedSingleton;
 
 class ArticleController extends Controller
 {
+    /*
+        Singleton example
+    */
     public function articles(){
-        $articleFacade = new ArticleFacade();
+        $article = ArticleExtendedSingleton::getInstance();
+        $article->setAllArticles(Article::all());
+        $data = $article->getAllArticles();
 
-        $data = $articleFacade->getAllArticles();
+        $article = ArticleExtendedSingleton::getInstance();
+        $article->setAllArticles(['faker' => []]);
+        $data = $article->getAllArticles();
         
-        return view('welcome', ['article' => $data]);
+        return view('welcome', ['article' => $data[0]]);
     }
 
     public function article(Request $request){        
