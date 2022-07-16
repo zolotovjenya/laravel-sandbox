21db6e7393476758626a2edc7ab2eb28b8bec3c7
zolotovjenya <zolotovjenya@gmail.com>
2022-07-16 12:55:45 +0300

    added test db for unit tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Article;
 
 class ApiTest extends TestCase
 {
@@ -15,8 +16,22 @@ class ApiTest extends TestCase
      */
     public function testCreateArticle()
     {
-        $response = $this->get('/');
+        $article = Article::factory()->create([
+            'id' => 1
+        ]);
+
+        $response = $this->get('/article/1');
 
         $response->assertStatus(200);
+    }
+
+    public function testRemoveArticle()
+    {
+        $article = Article::find(1)->first();
+        $article->delete();
+
+        $response = $this->get('/article/1');
+
+        $response->assertNotFound();
     }
 }
