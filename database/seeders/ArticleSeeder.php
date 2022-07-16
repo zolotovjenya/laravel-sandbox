55d4e35d1183ea1b2f1d60da17d1c12967dc19a9
zolotovjenya <zolotovjenya@gmail.com>
2022-07-16 11:29:22 +0300

    edited article seeder

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -14,40 +14,6 @@ class ArticleSeeder extends Seeder
      */
     public function run()
     {
-        /*$articles = [ 
-            [ 
-                'title' => 'Test1',
-                'content' => 'Content1',
-                'payment_type' => 'stripe',
-                'url' => 'test1',
-                'sponsor' => 'hieneken'
-            ],
-            [ 
-                'title' => 'Test2',
-                'content' => 'Content2',
-                'payment_type' => 'worldpay',
-                'url' => 'test2',
-                'sponsor' => 'umbro'
-            ], 
-            [ 
-                'title' => 'Test3',
-                'content' => 'Content3',
-                'payment_type' => 'uapay',
-                'url' => 'test3',
-                'sponsor' => 'huawei'
-            ],
-        ];
-
-        foreach($articles as $article){
-            Article::create([
-               'title' => $article['title'],
-               'content' => $article['content'],
-               'payment_type' => $article['payment_type'],
-               'url' => $article['url'],
-               'sponsor' => $article['sponsor']
-            ]);
-        }*/
-
         Article::factory(3)->create();
     }
 }
